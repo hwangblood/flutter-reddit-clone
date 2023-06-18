6060abeb6df987a23646f20be7ffb4ada0dc8b4e
hwangblood <hwangblood@gmail.com>
2023-06-18 14:49:54 +0800

    Home Screen AppBar UI

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -9,10 +9,32 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final user = ref.watch(userProvider);
+    // it shouldn't be null, when user is logged in
+    final user = ref.watch(userProvider)!;
+
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Home'),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage(user.profilePic),
+            ),
+          ),
+        ],
+      ),
       body: Center(
-        child: Text(user!.name),
+        child: Text(user.name),
       ),
     );
   }
